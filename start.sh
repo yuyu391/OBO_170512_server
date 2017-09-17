1803c1be1a671efeb8dec1db483fb6a67a3c97a1
aceld <danbing_at@163.com>
2017-09-17 15:28:33 +0800

    添加启动之前 杀死进程

--- start.sh
+++ start.sh
@@ -1,7 +1,12 @@
 
+kill -9 `ps aux|grep "https_web_server"| grep -v "grep"|awk '{print $2}'`
+kill -9 `ps aux|grep "https_data_server"| grep -v "grep"|awk '{print $2}'`
+
+
+
 cd  ./https_web_server/
-./server &
+./https_web_server &
 cd ..
 cd ./https_data_server/
-./server &
+./https_data_server &
 cd ..
